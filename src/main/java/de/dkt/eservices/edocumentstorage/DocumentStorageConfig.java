8b9023fd8197e3d5bfba0bc2b54079c6eced4948
jnehring <gmnoname@gmx.de>
2017-01-16 17:19:38 +0100

    remove ssl stuff because it did not fix the issue

--- src/main/java/de/dkt/eservices/edocumentstorage/DocumentStorageConfig.java
+++ src/main/java/de/dkt/eservices/edocumentstorage/DocumentStorageConfig.java
@@ -43,9 +43,6 @@ public class DocumentStorageConfig {
 		if( !documentStorageLocation.exists() && !documentStorageLocation.mkdirs()){
 			throw new RuntimeException(String.format("Cannot create storage directory \"%s\"", documentStorageLocation)); 
 		}		
-		
-		// set ssl protokoll
-		System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
 	}
 	
 	public File getDocumentStorageLocation(){
